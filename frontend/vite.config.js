5592f1a2d761bba88bbfa756e130b89d5ae1ddea
LavoisieSunny <lavoisiesunnycourse@gmail.com>
2026-06-09 17:01:47 +0530

    Update frontend port to 3106

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3101,
+    port: 3106,
     proxy: {
       "/api": {
         target: "http://localhost:3110",
